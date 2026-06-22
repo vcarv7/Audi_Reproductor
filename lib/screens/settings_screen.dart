9a137643520c5f8136aee6d442bedad5d22f09b1
vcarv7 <victorcv326@gmail.com>
2026-06-22 14:59:39 -0400

    feat: Remove exit option from settings screen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -157,12 +156,6 @@ class _SettingsScreenState extends State<SettingsScreen> {
                           await launchUrl(uri);
                         }
                       },
-                    ),
-                    _SettingsTile(
-                      icon: Icons.exit_to_app_rounded,
-                      title: 'Salir de la app',
-                      subtitle: 'Cerrar EcoPlayer',
-                      onTap: () => SystemNavigator.pop(),
                     ),
                     const SizedBox(height: 32),
                     Center(
